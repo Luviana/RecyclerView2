122ef8a5b9dfe53d6c42df8d1089c0787aaa3d10
Luviana <lutfianadewi5@gmail.com>
2016-11-03 21:06:59 +0700

    RecyclerView Grid

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/adapter/HotelAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/adapter/HotelAdapter.java
@@ -38,7 +38,6 @@ import id.sch.smktelkom_mlg.learn.recyclerview2.model.Hotel;
         {
             Hotel hotel = hotelList.get(position);
             holder.tvJudul.setText(hotel.judul);
-            holder.tvDeskripsi.setText(hotel.deskripsi);
             holder.ivFoto.setImageDrawable(hotel.foto);
         }
 
@@ -54,14 +53,12 @@ import id.sch.smktelkom_mlg.learn.recyclerview2.model.Hotel;
         {
             ImageView ivFoto;
             TextView tvJudul;
-            TextView tvDeskripsi;
 
             public ViewHolder(View itemView)
             {
                 super(itemView);
                 ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
                 tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
-                tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
             }
         }
     }
